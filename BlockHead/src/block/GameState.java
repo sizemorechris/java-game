2eb17b57e1510cc914f483940bad31f13926318a
Christopher Sizemore <sizemore.chris@gmail.com>
2014-10-12 21:02:42 -0400

    fixed animation
    fixed animation, still need to implement jump

--- BlockHead/src/block/GameState.java
+++ BlockHead/src/block/GameState.java
@@ -14,9 +14,9 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class GameState extends BasicGameState{
 	
-	//private Rectangle block;
 	private SpriteSheet blockySheet;
 	private Animation blockyAnimation;
+	private boolean isJumping;
 	private int dX;
 	private int dY;
 
@@ -25,6 +25,7 @@ public class GameState extends BasicGameState{
 			throws SlickException {
 		blockySheet = new SpriteSheet("res/avatarss.png", 50, 100);
 		blockyAnimation = new Animation(blockySheet, 150);
+		isJumping = false;
 		dX = 0;
 		dY = 0;
 	}
@@ -34,28 +35,46 @@ public class GameState extends BasicGameState{
 			throws SlickException {
 		container.setVSync(true);
 		g.setBackground(Color.white);
-		blockyAnimation.draw(100, 100);
+		
+		blockyAnimation.draw(400 + dX, 450 + dY);
 		
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int delta)
-			throws SlickException {	
-		/*if (container.getInput().isKeyDown(Input.KEY_LEFT)) {
-			if (400 + dX > 50) {
-				dX -= 4;
+			throws SlickException {
+		blockyAnimation.stop();
+		if (container.getInput().isKeyDown(Input.KEY_LEFT)) {
+			if (!isJumping) {
+				if (400 + dX > 50) {
+					blockyAnimation.start();
+					dX -= 4;
+				}
+			} else {
+				if (400 + dX > 50) {
+					blockyAnimation.stop();
+					dX -= 3;
+				}
 			}
 		} 
 		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) {
-			if (400 + dX < 750) {
-				dX += 4;
+			if (!isJumping) {
+				if (400 + dX < 750) {
+					blockyAnimation.start();
+					dX += 4;
+				}
+			} else {
+				if (400 + dX < 750) {
+					blockyAnimation.stop();
+					dX += 3;
+				}
 			}
-		}*/
+		}
+		if (container.getInput().isKeyDown(Input.KEY_SPACE) || container.getInput().isKeyDown(Input.KEY_UP) && !isJumping) {
+			
+		}
+	
 		blockyAnimation.update(delta);
-		
-		//block.setCenterX(400 + dX);
-		//block.setCenterY(450 + dY);
-		
 	}
 
 	@Override
